3f05df4494905341d00b8ba91a6cccf44e59ce76
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-06 16:13:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+module "lambda_layers" {
+  source = "./lambda-layers
+  layer_names = [    "layer1",    "layer2" ]
+  layer_s3_bucket = "bskhatrig-testing"
+  layer_s3_keys = [    "layer1.zip",    "layer2.zip" ]
+}
