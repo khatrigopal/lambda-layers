d16b93fd0466fcfad7517b67a4b37bd93f91f445
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-06 16:33:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "lambda_layers" {
-  source = "./lambda-layers
+  source = "./lambda-layers"
   layer_names = [    "layer1",    "layer2" ]
   layer_s3_bucket = "bskhatrig-testing"
   layer_s3_keys = [    "layer1.zip",    "layer2.zip" ]
